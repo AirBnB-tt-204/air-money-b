87c56445c54525e91d8f66ed2fc12db03ce452c8
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-02-25 21:40:59 -0800

    Fixed flake8 warnings.

--- airbnb_app/__init__.py
+++ airbnb_app/__init__.py
@@ -4,7 +4,6 @@ Create and initialize Flask app and SQLAlchemy database.
 import os
 import psycopg2
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
 from models import DB, init_db
@@ -13,14 +12,15 @@ from routes import airbnb_routes
 load_dotenv()
 
 
-# database_uri = 'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+# DB_URI = 'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 
-DB_URI = 'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'.format(
-    DB_NAME = os.getenv("DB_NAME"),
-    DB_HOST = os.getenv("DB_HOST"),
-    DB_USER = os.getenv("DB_USER"),
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-)
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+DB_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
 
 def create_app():
     '''
