83f54cb9030866535030d5277b69252ec90e4320
TemsyChen <70554318+TemsyChen@users.noreply.github.com>
2021-02-25 22:19:16 -0800

    Added Heroku PostgreSQL

--- airbnb_app/__init__.py
+++ airbnb_app/__init__.py
@@ -17,19 +17,19 @@ gunicorn_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
 
-'''Connects to Postgres database'''
-DB_URI = 'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'.format(
-    DB_NAME = os.getenv("DB_NAME"),
-    DB_HOST = os.getenv("DB_HOST"),
-    DB_USER = os.getenv("DB_USER"),
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-)
+# '''Connects to Elephant Postgres database'''
+# DB_URI = 'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'.format(
+#     DB_NAME = os.getenv("DB_NAME"),
+#     DB_HOST = os.getenv("DB_HOST"),
+#     DB_USER = os.getenv("DB_USER"),
+#     DB_PASSWORD = os.getenv("DB_PASSWORD")
+# )
 
 def create_app():
 
     app.secret_key = os.urandom(42)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     DB.init_app(app)
     
